42250a377d77daa555af3ef3f734fb2ec62777f7
Vassili Altynikov <altynikov@gmail.com>
2016-01-05 22:29:07 -0600

    Update test-file.cs

--- test-dir/test-file.cs
+++ test-dir/test-file.cs
@@ -1,19 +1,14 @@
 public class Test
 {
-  private string _str1;
-  public string Str1
-  {
-    get { return _str1; }
-    set { _str1 = value; }
-  }
+  public string Str1 { get; set; }
   
   public Test()
   {
-    _str1 = null;
+    
   }
   
   public Test(string str1)
   {
-    _str1 = str1;
+    Str1 = str1;
   }
 }
